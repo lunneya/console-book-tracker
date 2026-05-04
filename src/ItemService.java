9ac71d006c5a0afb0a4f0df2a582f8ffddf660d0
lunneya <lunneya.nivarre@gmail.com>
2026-05-04 19:47:51 +0500

    I moved the logic to ItemService and worked with returning and displaying the list.

--- src/ItemService.java
+++ src/ItemService.java
@@ -1,3 +1,14 @@
-public class ItemService {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ItemService {
+    private List<Item> items = new ArrayList<>();
+
+    void addItem(Item item) {
+        items.add(item);
+    }
+
+    public List<Item> getAllItems() {
+        return new ArrayList<>(items);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
 
         // Создание сканнера для прочтение данных, что вводит пользователь
         Scanner scanner = new Scanner(System.in);
@@ -9,17 +11,21 @@ public class Main {
         // Читаем введенные данные
         System.out.println("Название:");
         String title = scanner.nextLine();
-
         System.out.println("Тип:");
         String type = scanner.nextLine();
-
         System.out.println("Рейтинг:");
         int rating = scanner.nextInt();
+        scanner.nextLine();
 
-        // Создаем класс для вывода данных
-        Item items = new Item(title, type, rating);
+        ItemService service = new ItemService();
+        Item item = new Item(title, type, rating);
+        service.addItem(item);
 
-        // Вывод данных
-        System.out.printf("Title: %s, Type: %s, Rating: %d%n", items.getTitle(), items.getType(), items.getRating());
+        List<Item> allItems = service.getAllItems();
+        for (Item currentItem : allItems) {
+            System.out.printf("Title: %s, Type: %s, Rating: %d%n",
+                    currentItem.getTitle(), currentItem.getType(), currentItem.getRating()
+            );
+        }
     }
 }
